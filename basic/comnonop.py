e49f7c2e5547c22916ecad8f594e204409dc76b4
ZB <1066816806@qq.com>
2018-11-19 10:17:24 +0800

    gpu 配置项

--- basic/comnonop.py
+++ basic/comnonop.py
@@ -2,3 +2,39 @@
 start=(i*batch_size)%dataset
         end=min(start+batch_size,dataset)
         sess.run(train_step,feed_dict={x:X[start:end],y_true:Y[start:end]})
+		
+		
+# GPU配置
+
+with tf.Session(config = tf.ConfigProto(...),...)
+1
+#tf.ConfigProto()的参数
+log_device_placement=True : 是否打印设备分配日志
+allow_soft_placement=True ： 如果你指定的设备不存在，允许TF自动分配设备
+tf.ConfigProto(log_device_placement=True,allow_soft_placement=True)
+
+
+#allow growth
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config, ...)
+# 使用allow_growth option，刚一开始分配少量的GPU容量，然后按需慢慢的增加，由于不会释放
+#内存，所以会导致碎片
+
+# per_process_gpu_memory_fraction
+gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=0.7)
+config=tf.ConfigProto(gpu_options=gpu_options)
+session = tf.Session(config=config, ...)
+#设置每个GPU应该拿出多少容量给进程使用，0.4代表 40%
+
+
+
+控制使用哪块GPU
+CUDA_VISIBLE_DEVICES=0  python your.py#使用GPU0
+CUDA_VISIBLE_DEVICES=0,1 python your.py#使用GPU0,1
+#注意单词不要打错
+
+#或者在 程序开头
+os.environ['CUDA_VISIBLE_DEVICES'] = '0' #使用 GPU 0
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,1' # 使用 GPU 0，1
+
